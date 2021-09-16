d27dca7dc155781cdb52af457e8a2e671f5d290e
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2021-09-16 20:36:20 +0300

    include code from node_modules into bundle

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+import compose from 'lodash/fp/compose';
+import filter from 'lodash/fp/filter';
+import map from 'lodash/fp/map';
+
 import { A } from './moduleA';
 import { B } from './moduleB';
 
 let a = new A;
 let b = new B;
+
+compose(
+  filter(x => x > 0),
+  map(x => x * 2)
+)([-1, 0, 1]);
